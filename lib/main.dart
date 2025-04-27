ee86a64a3a940102f1614a6db070cd0c851e03a8
Hemraj Soyal <hemrajsoyal10@gmail.com>
2025-04-27 23:58:49 +0530

    Setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:utavine/core/configs/theme/app_theme.dart';
+import 'package:utavine/firebase_options.dart';
 import 'package:utavine/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:utavine/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorageDirectory.web
-        : HydratedStorageDirectory((await getApplicationDocumentsDirectory()).path),
+    storageDirectory:
+        kIsWeb
+            ? HydratedStorageDirectory.web
+            : HydratedStorageDirectory(
+              (await getApplicationDocumentsDirectory()).path,
+            ),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); 
   runApp(MyApp());
 }
 
@@ -22,19 +28,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-           create: (_) => ThemeCubit(),
-        ),
-      ],
+      providers: [BlocProvider(create: (_) => ThemeCubit())],
       child: BlocBuilder<ThemeCubit, ThemeMode>(
-        builder: (context, mode) => MaterialApp(
-          theme: AppTheme.darkTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: mode,
-          debugShowCheckedModeBanner: false,
-          home: const SplashPage(),
-        ),
+        builder:
+            (context, mode) => MaterialApp(
+              theme: AppTheme.darkTheme,
+              darkTheme: AppTheme.darkTheme,
+              themeMode: mode,
+              debugShowCheckedModeBanner: false,
+              home: const SplashPage(),
+            ),
       ),
     );
   }
